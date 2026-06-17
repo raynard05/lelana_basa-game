67dc78fb831b9cadeea301667e2f05148145f51e
panggilsajarey <150323912+raynard05@users.noreply.github.com>
2026-06-17 22:03:50 +0700

    add fix final

--- app/babak1/page4/page.tsx
+++ app/babak1/page4/page.tsx
@@ -192,7 +192,7 @@ export default function Babak1Page4() {
     const words = text.split(/\s+/).map(normalizeWord).filter(Boolean);
 
     // Allowed target words (supporting both 'amerga' and 'amarga' spelling)
-    const targetWords = ['aku', 'luput', 'apa', 'marang', 'kowe', "opo", "salahku", "nyapo", "gak", "gelem", "dadi", "kancaku", "iyo", "wis", "salah", 'iya', 'Ora', "kancoku", "yo"];
+    const targetWords = ['aku', 'luput', 'apa', 'marang', 'kowe', "opo", "salahku", "nyapo","ogak", "gak", "gelem", "dadi", "kancaku", "iyo", "wis", "salah", 'iya', 'Ora', "kancoku", "yo"];
     // Check count of target words spoken
     const uniqueMatched = Array.from(new Set(words.filter(word => targetWords.includes(word))));
     const matchedCount = uniqueMatched.length;
