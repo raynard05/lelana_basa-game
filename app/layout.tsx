3213419ebe5294a82856c4793a8668444cf074ce
panggilsajarey <150323912+raynard05@users.noreply.github.com>
2026-07-10 22:18:26 +0700

    add fix translate

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="id"
+      translate="no"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased notranslate`}
     >
       <body className="min-h-full flex flex-col">
         <OrientationGuard />
