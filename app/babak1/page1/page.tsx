d8424cd76c1c8934c934f35544ba4f4085967560
panggilsajarey <150323912+raynard05@users.noreply.github.com>
2026-09-12 08:43:20 +0700

    add revisi 4

--- app/babak1/page1/page.tsx
+++ app/babak1/page1/page.tsx
@@ -142,7 +142,7 @@ export default function Babak1Page1Page() {
     if (typeof window !== 'undefined') {
       localStorage.setItem('game_score', '0');
       localStorage.setItem('game_streak', '0');
-      localStorage.removeItem('game_ulasan');
+      localStorage.removeItem('ulasan_materi');
       
       Object.keys(localStorage).forEach(key => {
         if (key.includes('_timer_')) {
